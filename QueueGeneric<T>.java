aba5e7933efe8126b91c2670b43f7e02a16fae88
dwika12 <44666489+dwika12@users.noreply.github.com>
2018-11-01 17:43:24 +0800

    Create QueueGeneric<T>.java

---
+++ QueueGeneric<T>.java
@@ -0,0 +1,37 @@
+package shatdwikamadaniputri;
+
+import java.util.Vector;
+
+public class QueueGeneric<T> {
+	  private Vector <T> queue = new Vector<T>();
+	  public int top = -1;
+	    public QueueGeneric()
+
+	    {    
+	    }
+
+	    public void insert (T value)
+
+	    {
+	    		top++;
+	          queue.add(top,value);
+
+	    }
+
+	    public int remove()
+
+	    {
+	    	int value = top;
+	        if (top>-1){
+	        value = (Integer) queue.get(0);
+				queue.remove(0);
+				top--;
+			}
+			return value;
+	            //return null;
+	     //   }
+
+	       // return queue.remove(0);
+
+	    }
+}
