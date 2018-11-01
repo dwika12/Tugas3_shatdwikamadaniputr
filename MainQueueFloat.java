f29cf6c89c50b4f71f057b4190a8e2b8e6bb6736
dwika12 <44666489+dwika12@users.noreply.github.com>
2018-11-01 17:26:20 +0800

    Create MainQueueFloat.java

---
+++ MainQueueFloat.java
@@ -0,0 +1,16 @@
+package shatdwikamadaniputri;
+
+public class MainQueueFloat {
+
+	public static void main(String[] args) {
+
+		QueueFloat Ha = new QueueFloat();
+		Ha.cetak();
+		
+		Ha.insert(1.1f);Ha.cetak();
+		Ha.insert(2.1f);Ha.cetak();
+		Ha.insert(3.1f);Ha.cetak();
+		Ha.insert(4.1f);Ha.cetak();
+
+}
+	}
