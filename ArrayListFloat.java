51580f11ca5ab397d9afe76f3bcbcd2144f4050a
dwika12 <44666489+dwika12@users.noreply.github.com>
2018-11-01 16:58:23 +0800

    Create ArrayListFloat.java

---
+++ ArrayListFloat.java
@@ -0,0 +1,17 @@
+package shatdwikamadaniputri;
+
+import java.util.ArrayList;
+
+public class ArrayListFloat {
+
+	public static void main(String[] args) {
+		ArrayList<Float> q = new ArrayList<Float>();
+		
+		q.add(1.2f);
+		q.add(2.3f);
+		q.add(3.4f);
+		System.out.println(" : "+q.toString());
+		System.out.println("SIZE : "+q.size());
+	}
+
+}
