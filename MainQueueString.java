0f34f2a2f5799786a194132f9c339d5f68de3f7f
dwika12 <44666489+dwika12@users.noreply.github.com>
2018-11-01 17:28:45 +0800

    Create MainQueueString.java

---
+++ MainQueueString.java
@@ -0,0 +1,16 @@
+package shatdwikamadaniputri;
+
+public class MainQueueString {
+
+	public static void main(String[] args) {
+		QueueString Ha = new QueueString();
+		Ha.cetak();
+		
+		Ha.insert("Afri");Ha.cetak();
+		Ha.insert("Ganteng");Ha.cetak();
+		Ha.insert("Sekali");Ha.cetak();
+		Ha.insert("Donn");Ha.cetak();
+
+	}
+	}
+
