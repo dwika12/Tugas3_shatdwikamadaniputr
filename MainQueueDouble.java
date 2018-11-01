d4bb52c40abd2eb1ea3c41ea5bbd5b0bfb00a463
dwika12 <44666489+dwika12@users.noreply.github.com>
2018-11-01 17:24:45 +0800

    Create MainQueueDouble.java

---
+++ MainQueueDouble.java
@@ -0,0 +1,17 @@
+package shatdwikamadaniputri;
+
+public class MainQueueDouble {
+
+	public static void main(String[] args) {
+
+		QueueDouble Ha = new QueueDouble();
+		Ha.cetak();
+		
+		Ha.insert(1.1);Ha.cetak();
+		Ha.insert(2.1);Ha.cetak();
+		Ha.insert(3.1);Ha.cetak();
+		Ha.insert(4.1);Ha.cetak();
+		
+
+	}
+	}
